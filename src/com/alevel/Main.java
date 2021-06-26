d3d094da67197c5140e5262ff18194f4893beb69
Tod <valerafatkulin7@gmail.com>
2021-06-26 12:31:36 +0300

    hw

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -3,9 +3,11 @@ package com.alevel;
 public class Main {
 
     public static void main(String[] args) {
-	StringArray toto = new StringArray("ab","ad","aj", "aa", "af", "ca");
+	StringArray toto = new StringArray("1","4","2", "8", "9", "7");
 	toto.value();
 	toto.leftRight();
+	toto.value();
+	toto.righLeft();
 	toto.value();
     }
 }
--- src/com/alevel/StringArray.java
+++ src/com/alevel/StringArray.java
@@ -11,13 +11,15 @@ public class StringArray {
     public void value(){
         String mus = Arrays.toString(array);
         System.out.println(mus + "; ");
+        for (int f = 1; f <= array.length; f++) {
+            String rot = array[f-1];
+            System.out.print(f + " number is " + rot + "; ");
+        }System.out.println();
     }
     public void leftRight(){
-        for (int z=0; z< array.length-1; z++) {
-            //array[z].toString();
             for (int k = 1; k < array.length; k++) {
                 String sort = array[k];
-                System.out.println("sort " + sort);
+               // System.out.println("sort " + sort);
                 int j = k;
                 while (j > 0 && (array[j - 1].compareTo(sort)>=0)) {
                     this.array[j] = this.array[j - 1];
@@ -25,20 +27,24 @@ public class StringArray {
                 }
                 this.array[j] = sort;
             }
+    }
 
-        }
-       /* String mus = Arrays.toString(array);
-        System.out.println(mus + "; ");*/
-        /*for (int left = 1; left< array.length-1; left++){
-            String value = array[left];
-            int l = left-1;
-            for (; l >= 0; l--){
-                if (array[left].compareTo(array[l])>=0){
-                    array[l+1] = array[l];
-                }else {
-                    break;
+    public void righLeft(){
+        for (int y = 0; y < array.length/2; y++) {
+            String tmp = array[y];
+            array[y] = array[array.length - y - 1];
+            array[array.length - y - 1] = tmp;
+            for (int k = 1; k < array.length; k++) {
+                String sort = array[k];
+              //  System.out.println("sort " + sort);
+                int j = k;
+                while (j > 0 && (array[j - 1].compareTo(sort)<=0)) {
+                    this.array[j] = this.array[j - 1];
+                    j--;
                 }
-            }array[l+1] = value;
-        }*/
+                this.array[j] = sort;
+            }
+        }
+
     }
 }
