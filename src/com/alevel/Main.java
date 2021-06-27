44131d096d18d354314fb343ca409bb9ab9c1351
Tod <valerafatkulin7@gmail.com>
2021-06-27 23:28:47 +0300

    homework

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -3,11 +3,7 @@ package com.alevel;
 public class Main {
 
     public static void main(String[] args) {
-	StringArray string = new StringArray("1","4","2", "8", "9", "7");
-	string.value();
-	string.leftRight();
-	string.value();
-	string.righLeft();
-	string.value();
-    }
+    	System.out.println("It is my homework");
+	}
+
 }
